735bcccbbcc6089f8dff1eafc8c8c27c01a2ca92
tharun <tharunbollam66@gmail.com>
2025-12-17 18:40:36 +0530

    cors error solved

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(helmet());
 app.use(limiter);
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://code-snippet-sand.vercel.app/'] 
+    ? ['https://code-snippet-sand.vercel.app'] 
     : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
